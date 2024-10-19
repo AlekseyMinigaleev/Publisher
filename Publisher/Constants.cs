3940d48f8a71974efbb011339e56cb039fbd4151
Aleksey Minigaleev <Aleksey.Minigaleev@gmail.com>
2024-10-19 16:13:45 +0500

    #1 delete constants for tests

--- Publisher/Constants.cs
+++ Publisher/Constants.cs
@@ -14,32 +14,8 @@
         public const string SLN = ".sln";
     }
 
-    public static class DirectoryNameConstants
-    {
-        public const string NEW_FOLDER = "new folder";
-
-        public const string BUILDS = "Builds";
-
-        public const string ALL_PROJECT_SOLUTION = "AllProjectSolution";
-
-        public const string WPF_SOLUTION = "WpfProjectSolution";
-    }
-
     public static class CsprojLinesConstants
     {
         public const string PROJECT = "Project(";
-    }
-
-    public static class DirectoryPathConstatns
-    {
-        public static readonly string BASE_PATH = Utils.GetBaseDirectory();
-
-        public static readonly string ALL_PROJECT_SOLUTION = $"{BASE_PATH}{DirectoryNameConstants.ALL_PROJECT_SOLUTION}";
-
-        public static readonly string WPF_PROJECT_SOLUTION = $"{BASE_PATH}{DirectoryNameConstants.WPF_SOLUTION}";
-
-        public static readonly string BUILDS = $"{BASE_PATH}{DirectoryNameConstants.BUILDS}";
-
-        public static readonly string OUTPUT = $"{BASE_PATH}{DirectoryNameConstants.BUILDS}";
     }
 }
